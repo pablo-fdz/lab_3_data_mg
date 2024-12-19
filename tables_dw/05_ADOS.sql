e9fa55801341d150ad8b0c59771be18920d61502
pablo-fdz <pablo.fdzalb@gmail.com>
2024-12-19 16:45:31 +0100

    Converted number_days to float

--- tables_dw/05_ADOS.sql
+++ tables_dw/05_ADOS.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS ADOS CASCADE;
 CREATE TABLE ADOS (
     time_id VARCHAR(50),
     aircraft_id VARCHAR(50),
-    number_days INT NOT NULL,
+    number_days FLOAT NOT NULL,
     PRIMARY KEY (time_id, aircraft_id),
     FOREIGN KEY (time_id) REFERENCES Time(time_id),
     FOREIGN KEY (aircraft_id) REFERENCES Aircraft(registration_number)
