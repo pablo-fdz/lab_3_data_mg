09f7f5de80d00fdc9864749c0023c6a3438c708b
pablo-fdz <pablo.fdzalb@gmail.com>
2024-12-19 16:23:16 +0100

    Removed not null from Flights table (flight cycles)

--- tables_dw/03_Flights.sql
+++ tables_dw/03_Flights.sql
@@ -4,7 +4,7 @@ CREATE TABLE Flights (
     time_id VARCHAR(50),
     aircraft_id VARCHAR(50),
     total_flight_hours INT NOT NULL,
-    total_flight_cycles INT NOT NULL,
+    total_flight_cycles INT,
     PRIMARY KEY (time_id, aircraft_id),
     FOREIGN KEY (time_id) REFERENCES Time(time_id),
     FOREIGN KEY (aircraft_id) REFERENCES Aircraft(registration_number)
